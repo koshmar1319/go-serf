59b0ca60abc539245441cf6111fc6718a09da7ec
Kirill <koshmar1319@mail.ru>
2020-08-24 12:23:21 +0300

    added section shop and footer

--- app/js/main.js
+++ app/js/main.js
@@ -30,7 +30,7 @@ $(function(){
     focusOnSelect: true
   });
 
-  $('.holder__slider').slick({
+  $('.holder__slider, .shop__slider').slick({
     infinite: true,
     fade: true,
     prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt=""></img>',
@@ -77,5 +77,9 @@ $(function(){
 
   let summ = $('.nights').val() * $('.summ').data('nights') + ($('.guests').val() - 1) * $('.summ').data('guests');
   $('.summ').html('$' + summ);
+
+  $('.surfboard-box__circle').on('click', function(){
+    $(this).toggleClass('active')
+  });
 
 });
