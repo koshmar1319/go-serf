a3af8d78a7d6f6f9690e4ea92ed2ca10cf385324
Kirill <koshmar1319@mail.ru>
2020-08-26 18:00:09 +0300

    added adaptive section surf

--- app/js/main.js
+++ app/js/main.js
@@ -12,7 +12,12 @@ $(function(){
     slidesToShow: 4,
     slidesToScroll: 4,
     asNavFor: '.header__slider',
-    
+    responsive: [
+      {
+        breakpoint: 961,
+        settings: "unslick"
+      },
+    ]
   });
 
   $('.surf-slider').slick({
@@ -21,6 +26,34 @@ $(function(){
     prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt=""></img>',
     nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt=""></img>',
     asNavFor: '.slider-map',
+    responsive: [
+      {
+        breakpoint: 1210,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 720,
+        settings: {
+          slidesToShow: 1,
+          centerMode: true,
+        }
+      },
+      {
+        breakpoint: 426,
+        settings: {
+          slidesToShow: 1,
+          centerMode: false,
+        }
+      },
+    ]
   });
 
   $('.slider-map').slick({
@@ -28,7 +61,29 @@ $(function(){
     slidesToScroll: 1,
     arrows: false,
     asNavFor: '.surf-slider',
-    focusOnSelect: true
+    focusOnSelect: true,
+    responsive: [
+      {
+        breakpoint: 1103,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+          centerMode: true,
+        }
+      },
+      {
+        breakpoint: 720,
+        settings: {
+          slidesToShow: 1,
+          centerMode: true,
+        }
+      },
+    ]
   });
 
   $('.holder__slider, .shop__slider').slick({
